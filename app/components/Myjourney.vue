7192b6c80dc04118b9ef18d70be2cf19fa987e48
Takina <1132959964@qq.com>
2025-08-31 19:31:02 +0800

    modify myjourney

--- app/components/Myjourney.vue
+++ app/components/Myjourney.vue
@@ -10,7 +10,7 @@ const timelineItems: TimelineItem[] = [
   {
     date: '2025.08',
     title: '整理并复盘第一段职场经历，规划下一步的方向',
-    description: '我一读再读，却不得不说青春是本太仓促的书。'
+    description: '这一世，我要夺回属于我的一切。'
   },
   {
     date: '2024.06',
@@ -20,17 +20,17 @@ const timelineItems: TimelineItem[] = [
   {
     date: '2023.12',
     title: '第一次合作',
-    description: '与kai合作开始开发项目，创立了Apex相关道具店铺，制作发布各种攻略前瞻内容等，独立提升运营能力。'
+    description: '与kai合作开始开发游戏周边项目，创立了Apex线上相关道具店铺及网页站，制作发布各种攻略前瞻内容等，独立提升运营能力。'
   },
   {
     date: '2021.05',
     title: '单个视频50w播放，累计130w播放',
-    description: '且视他人疑目如盏盏鬼火，大胆地去走你的夜路。'
+    description: '船新的里程碑！！！'
   },
   {
     date: '2020.04.09',
     title: 'b站单个视频10w播放',
-    description: '对游戏的热爱有了数据的体现，这使我充满了决心。'
+    description: '对游戏的热爱有了不错的数据体现，这使我充满了决心。'
   },
   {
     date: '2020.04.07',
@@ -40,12 +40,12 @@ const timelineItems: TimelineItem[] = [
   {
     date: '2019.07',
     title: '正式接触编程开发相关内容',
-    description: '开始接触前端开发，学习了HTML、CSS和JavaScript，Mysql、Python等语言'
+    description: '开始接触开发，学习了HTML、CSS和JavaScript，Mysql、Python等语言。'
   },
   {
     date: '2013.10',
-    title: '第一个大型游戏，开启游戏探索之路',
-    description: '从洛克王国赛尔号这类网页游戏转到Minecraft、使命召唤等大型游戏'
+    title: '第一次开始深度体验的大型游戏，解锁游戏探索之路',
+    description: '从洛克王国赛尔号这类网页游戏转到Minecraft、使命召唤等大型游戏。'
   }
 ]
 </script>
